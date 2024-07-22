1270ce4b65ea4c8e46619b908c7a2bb46c6a5f35
Jace Wilson <poopnugget142@gmail.com>
2024-07-22 02:23:44 -0400

    fixed issues oops

--- src/hand.rs
+++ src/hand.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
-use leafwing_input_manager::prelude::*;
 use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
+use leafwing_input_manager::prelude::*;
 
 use crate::BoundingBox;
 
@@ -25,8 +25,7 @@ enum HandActions {
 const HAND_OFFSET: f32 = -500.0;
 
 pub(super) fn register(app: &mut App) {
-    app
-        .add_plugins(InputManagerPlugin::<HandActions>::default())
+    app.add_plugins(InputManagerPlugin::<HandActions>::default())
         .add_systems(Startup, spawn_hand)
         .add_systems(Update, (move_hand, grab, drop));
 }
@@ -36,7 +35,7 @@ fn spawn_hand(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
-){
+) {
     let texture = asset_server.load("ace_hearts.png");
 
     //Spawns card
@@ -69,8 +68,7 @@ fn drop(
     mut commands: Commands,
     hands: Query<(&ActionState<HandActions>, Entity, &Grabbing), With<CurrentHand>>,
     mut objects: Query<(&GlobalTransform, &mut Transform)>,
-)
-{
+) {
     if hands.is_empty() {
         return;
     }
@@ -100,10 +98,13 @@ fn grab(
     mut commands: Commands,
     //TODO! see if possible work around for this because it's annoying
     mut hands_and_objects: ParamSet<(
-        Query<(&ActionState<HandActions>, &Transform, Entity), (With<CurrentHand>, Without<Grabbing>)>,
+        Query<
+            (&ActionState<HandActions>, &Transform, Entity),
+            (With<CurrentHand>, Without<Grabbing>),
+        >,
         Query<(&BoundingBox, Entity, &mut Transform), With<Grabbable>>,
     )>,
-){
+) {
     let binding = hands_and_objects.p0();
 
     if binding.is_empty() {
@@ -134,13 +135,15 @@ fn move_hand(
     mut hands: Query<&mut Transform, With<CurrentHand>>,
     windows: Query<&mut Window>,
     cameras: Query<(&Camera, &GlobalTransform)>,
-){
+) {
     let mut transform = hands.single_mut();
     let window = windows.single();
     let (camera, camera_transform) = cameras.single();
 
-    if let Some(cursor_position) = window.cursor_position()
-    .and_then(|cursor| camera.viewport_to_world_2d(camera_transform, cursor)) {
+    if let Some(cursor_position) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world_2d(camera_transform, cursor))
+    {
         transform.translation = Vec3::new(cursor_position.x, cursor_position.y + HAND_OFFSET, 0.0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,65 +11,62 @@ use bevy_inspector_egui::quick::WorldInspectorPlugin;
 mod hand;
 
 #[derive(Component)]
-pub struct  ImageSize(Vec2);
+pub struct ImageSize(Vec2);
 
 #[derive(Component)]
 pub struct BoundingBox(Rect);
 
 fn main() {
     let mut app = App::new();
-        app.add_plugins(
-            DefaultPlugins.set(AssetPlugin {
-                // Wasm builds will check for meta files (that don't exist) if this isn't set.
-                // This causes errors and even panics in web builds on itch.
-                // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-                meta_check: AssetMetaCheck::Never,
-                ..default()}
-            ),
-        )
-        .add_systems(Startup, setup)
-        .add_systems(Update, (add_image_size, update_bounding_box));
+    app.add_plugins(DefaultPlugins.set(AssetPlugin {
+        // Wasm builds will check for meta files (that don't exist) if this isn't set.
+        // This causes errors and even panics in web builds on itch.
+        // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+        meta_check: AssetMetaCheck::Never,
+        ..default()
+    }))
+    .add_systems(Startup, setup)
+    .add_systems(Update, (add_image_size, update_bounding_box));
 
     hand::register(&mut app);
-
-    //on debug build
-    if cfg!(debug_assertions) {
-        app.add_plugins(WorldInspectorPlugin::new());
-    }
 
     app.run();
 }
 
-fn setup(
-    mut commands: Commands,
-) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
-
 
 //TODO! Only sets image size once
 fn add_image_size(
     mut commands: Commands,
-    mut sprites: Query<(&Transform, &Handle<Image>, Entity), (With<Sprite>, Without<ImageSize>, Changed<Handle<Image>>)>,
+    mut sprites: Query<
+        (&Transform, &Handle<Image>, Entity),
+        (With<Sprite>, Without<ImageSize>, Changed<Handle<Image>>),
+    >,
     assets: Res<Assets<Image>>,
 ) {
     for (transform, image_handle, entity) in sprites.iter_mut() {
         //TODO! possible crash on load with unwrap here! account for none!
         let image_dimensions = assets.get(image_handle).unwrap().size().as_vec2();
         let scaled_image_dimension = image_dimensions * transform.scale.truncate();
-        let bounding_box = Rect::from_center_size(transform.translation.truncate(), scaled_image_dimension);
+        let bounding_box =
+            Rect::from_center_size(transform.translation.truncate(), scaled_image_dimension);
 
-        commands.get_entity(entity).unwrap().insert(ImageSize(image_dimensions));
+        let mut e = commands.get_entity(entity).unwrap();
 
-        commands.get_entity(entity).unwrap().insert(BoundingBox(bounding_box));
+        e.insert(ImageSize(image_dimensions));
+
+        e.insert(BoundingBox(bounding_box));
     }
 }
 
-fn update_bounding_box(
-    mut sprites: Query<(&Transform, &ImageSize, &mut BoundingBox)>,
-) {
+fn update_bounding_box(mut sprites: Query<(&Transform, &ImageSize, &mut BoundingBox)>) {
     for (transform, image_size, mut bounding_box) in sprites.iter_mut() {
         let scaled_image_dimension = image_size.0 * transform.scale.truncate();
-        *bounding_box = BoundingBox(Rect::from_center_size(transform.translation.truncate(), scaled_image_dimension));
+        *bounding_box = BoundingBox(Rect::from_center_size(
+            transform.translation.truncate(),
+            scaled_image_dimension,
+        ));
     }
 }
