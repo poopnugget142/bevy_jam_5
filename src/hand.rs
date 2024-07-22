3cd19621ea2aaaeb3e2d987ea9a79909a00211ff
Jace Wilson <poopnugget142@gmail.com>
2024-07-22 02:03:32 -0400

    pickup / drop

---
+++ src/hand.rs
@@ -0,0 +1,146 @@
+use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
+
+use crate::BoundingBox;
+
+#[derive(Component)]
+pub struct Hand;
+
+#[derive(Component)]
+pub struct CurrentHand;
+
+#[derive(Component)]
+pub struct Grabbing(Entity);
+
+#[derive(Component)]
+pub struct Grabbable;
+
+#[derive(Actionlike, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
+enum HandActions {
+    Grab,
+    Move,
+}
+
+const HAND_OFFSET: f32 = -500.0;
+
+pub(super) fn register(app: &mut App) {
+    app
+        .add_plugins(InputManagerPlugin::<HandActions>::default())
+        .add_systems(Startup, spawn_hand)
+        .add_systems(Update, (move_hand, grab, drop));
+}
+
+fn spawn_hand(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+){
+    let texture = asset_server.load("ace_hearts.png");
+
+    //Spawns card
+    commands.spawn((
+        SpriteBundle {
+            texture,
+            transform: Transform::from_scale(Vec3::new(0.25, 0.25, -1.0)),
+            ..default()
+        },
+        Grabbable,
+    ));
+
+    //Input map
+    let input_map = InputMap::new([(HandActions::Grab, MouseButton::Left)]);
+
+    // Spawns the hand!
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(100.0, 1000.0))),
+            material: materials.add(Color::hsl(18.0, 0.57, 0.79)),
+            ..default()
+        },
+        InputManagerBundle::with_map(input_map),
+        Hand,
+        CurrentHand,
+    ));
+}
+
+fn drop(
+    mut commands: Commands,
+    hands: Query<(&ActionState<HandActions>, Entity, &Grabbing), With<CurrentHand>>,
+    mut objects: Query<(&GlobalTransform, &mut Transform)>,
+)
+{
+    if hands.is_empty() {
+        return;
+    }
+
+    let (action, hand, grabbing) = hands.single();
+
+    if !action.just_pressed(&HandActions::Grab) {
+        return;
+    }
+
+    let mut hand_commands = commands.entity(hand);
+
+    let (global_transform, mut transform) = objects.get_mut(grabbing.0).unwrap();
+
+    let stored_transform = global_transform;
+    hand_commands.remove_children(&[grabbing.0]);
+    hand_commands.remove::<Grabbing>();
+
+    let mut position = stored_transform.translation();
+
+    position.z = -1.0;
+
+    transform.translation = position;
+}
+
+fn grab(
+    mut commands: Commands,
+    //TODO! see if possible work around for this because it's annoying
+    mut hands_and_objects: ParamSet<(
+        Query<(&ActionState<HandActions>, &Transform, Entity), (With<CurrentHand>, Without<Grabbing>)>,
+        Query<(&BoundingBox, Entity, &mut Transform), With<Grabbable>>,
+    )>,
+){
+    let binding = hands_and_objects.p0();
+
+    if binding.is_empty() {
+        return;
+    }
+
+    let (action, transform, hand) = binding.single();
+
+    if !action.just_pressed(&HandActions::Grab) {
+        return;
+    }
+
+    let hand_spot = transform.translation + Vec3::new(0.0, -HAND_OFFSET, 0.0);
+
+    for (bounding_box, object, mut object_transform) in hands_and_objects.p1().iter_mut() {
+        // Clicked on grabble thing attach to hand
+        if bounding_box.0.contains(hand_spot.truncate()) {
+            let mut hand_commands = commands.entity(hand);
+            hand_commands.insert(Grabbing(object));
+            hand_commands.add_child(object);
+
+            object_transform.translation = Vec3::new(0.0, -HAND_OFFSET, 1.0);
+        }
+    }
+}
+
+fn move_hand(
+    mut hands: Query<&mut Transform, With<CurrentHand>>,
+    windows: Query<&mut Window>,
+    cameras: Query<(&Camera, &GlobalTransform)>,
+){
+    let mut transform = hands.single_mut();
+    let window = windows.single();
+    let (camera, camera_transform) = cameras.single();
+
+    if let Some(cursor_position) = window.cursor_position()
+    .and_then(|cursor| camera.viewport_to_world_2d(camera_transform, cursor)) {
+        transform.translation = Vec3::new(cursor_position.x, cursor_position.y + HAND_OFFSET, 0.0);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,70 @@
 
 use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+mod hand;
+
+#[derive(Component)]
+pub struct  ImageSize(Vec2);
+
+#[derive(Component)]
+pub struct BoundingBox(Rect);
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-            meta_check: AssetMetaCheck::Never,
-            ..default()
-        }))
+    let mut app = App::new();
+        app.add_plugins(
+            DefaultPlugins.set(AssetPlugin {
+                // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                // This causes errors and even panics in web builds on itch.
+                // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                meta_check: AssetMetaCheck::Never,
+                ..default()}
+            ),
+        )
         .add_systems(Startup, setup)
-        .run();
+        .add_systems(Update, (add_image_size, update_bounding_box));
+
+    hand::register(&mut app);
+
+    //on debug build
+    if cfg!(debug_assertions) {
+        app.add_plugins(WorldInspectorPlugin::new());
+    }
+
+    app.run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("ducky.png"),
-        ..Default::default()
-    });
+}
+
+
+//TODO! Only sets image size once
+fn add_image_size(
+    mut commands: Commands,
+    mut sprites: Query<(&Transform, &Handle<Image>, Entity), (With<Sprite>, Without<ImageSize>, Changed<Handle<Image>>)>,
+    assets: Res<Assets<Image>>,
+) {
+    for (transform, image_handle, entity) in sprites.iter_mut() {
+        //TODO! possible crash on load with unwrap here! account for none!
+        let image_dimensions = assets.get(image_handle).unwrap().size().as_vec2();
+        let scaled_image_dimension = image_dimensions * transform.scale.truncate();
+        let bounding_box = Rect::from_center_size(transform.translation.truncate(), scaled_image_dimension);
+
+        commands.get_entity(entity).unwrap().insert(ImageSize(image_dimensions));
+
+        commands.get_entity(entity).unwrap().insert(BoundingBox(bounding_box));
+    }
+}
+
+fn update_bounding_box(
+    mut sprites: Query<(&Transform, &ImageSize, &mut BoundingBox)>,
+) {
+    for (transform, image_size, mut bounding_box) in sprites.iter_mut() {
+        let scaled_image_dimension = image_size.0 * transform.scale.truncate();
+        *bounding_box = BoundingBox(Rect::from_center_size(transform.translation.truncate(), scaled_image_dimension));
+    }
 }
